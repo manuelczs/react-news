8f16896bee2a6c8196f67d1a96e83b6d2705b347
manuel <manuelczs@outlook.com>
2020-10-28 01:01:46 +0000

    server express deploy added

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (rea, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+});
+
+app.listen(4000)
