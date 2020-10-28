eb89e0ac57e04f58e8c340fedbf03411a51f344a
manuel <manuelczs@outlook.com>
2020-10-27 23:41:40 -0300

    headers set

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require('cors');
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
