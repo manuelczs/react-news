bdf1916b7d214f0429ea59edb32a0f3028bebb85
manuel <manuelczs@outlook.com>
2020-10-27 22:57:20 -0300

    CORS added app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(4000)
+app.listen(4000);
