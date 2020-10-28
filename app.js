e8f90507c2cd31e0db839debf1a21f94e3a3f4cf
manuel <manuelczs@outlook.com>
2020-10-28 01:34:23 +0000

    server for app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (rea, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
