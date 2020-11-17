9d04689bcbfb07755fe3f044fd5a077851b9560d
manuel <manuelczs@outlook.com>
2020-11-17 19:31:05 -0300

    path join used for public folder

--- news.js
+++ news.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
-
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
