c8f462740774f06b5e33539b2cbb4db646e70dfc
manuel <manuelczs@outlook.com>
2020-11-17 21:38:39 -0300

    public path removed, http changed by https in api request

--- news.js
+++ news.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(function (req, res, next) {
