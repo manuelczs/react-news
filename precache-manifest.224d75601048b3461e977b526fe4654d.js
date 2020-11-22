f23050bbbc35efa8e7fd38ac53b4ce02306df318
manuel <manuelczs@outlook.com>
2020-11-22 19:11:50 -0300

    Updates

---
+++ precache-manifest.224d75601048b3461e977b526fe4654d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2d4b0e218ecd79c39f93d4cbaf453b",
+    "url": "/react-news/index.html"
+  },
+  {
+    "revision": "8110a671e3046961bf93",
+    "url": "/react-news/static/css/main.e5a288bb.chunk.css"
+  },
+  {
+    "revision": "065462c000629cb9adb8",
+    "url": "/react-news/static/js/2.7d53cd1c.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/react-news/static/js/2.7d53cd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8110a671e3046961bf93",
+    "url": "/react-news/static/js/main.6ebde566.chunk.js"
+  },
+  {
+    "revision": "98d257c57d1eaa55b94d",
+    "url": "/react-news/static/js/runtime-main.715d2bb7.js"
+  }
+]);
