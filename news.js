8dad16a650ec9e18b0bf30e3b454216737e46ff4
Manuel <manuelczs@outlook.com>
2021-08-05 20:34:31 -0300

    ES6 changes

--- news.js
+++ news.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import api from './api/server';
 const app = express();
-const api = require('./api/server');
 const port = 4000;
 
 app.use(express.static(path.join(__dirname, 'build')));
