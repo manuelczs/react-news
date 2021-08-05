6ce373cf62731d28fe0dc9836db204ca90e67faa
Manuel <manuelczs@outlook.com>
2021-08-05 20:53:23 -0300

    using process.env.PORT

--- news.js
+++ news.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./api/server');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
