764ad70753d2b57c390595abd56fa242e7307af4
Manuel <manuelczs@outlook.com>
2021-08-05 22:18:55 -0300

    port 4000 deleted

--- news.js
+++ news.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./api/server');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
